8345c97117cfb2a538a9eecb6745c2b5815296cb
ryrie <ryrie106@gmail.com>
2019-10-10 22:47:39 +0900

    github page 설정 변경

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const VIDFLOW_URL = process.env.VIDFLOW_BASE_URL || 'http://localhost:8080';
+export const VIDFLOW_URL = process.env.VIDFLOW_BASE_URL || 'https://vidflow.ryrie.xyz:8080';
 
 export const ACCESS_TOKEN = 'accessToken';
 
