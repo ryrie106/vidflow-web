d1259a78ac445a8a801bc2ead321f06156170f24
ryrie <ryrie106@gmail.com>
2020-03-11 19:42:23 +0900

    travis.ci 설정 추가

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
-export const API_ROOT_URL = 'https://vidflow.ryrie.xyz:8080';
+export const API_ROOT_URL = 'https://api.vidflow.ryrie.xyz:8080';
 // export const API_ROOT_URL = 'http://localhost:8080';
 
 export const ACCESS_TOKEN = 'accessToken';
 
-export const VIDEO_SRC = 'https://s3.ap-northeast-2.amazonaws.com/vidflow/videos/';
-export const IMAGE_SRC = 'https://s3.ap-northeast-2.amazonaws.com/vidflow/images/';
+export const VIDEO_SRC = 'https://vidflow-s3.s3.ap-northeast-2.amazonaws.com/videos/';
+export const IMAGE_SRC = 'https://vidflow-s3.s3.ap-northeast-2.amazonaws.com/images/';
 
 export const PAGE_SIZE = 3;
 
